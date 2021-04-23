7654ef5824faf0246ba19d7de6f55df139b554ec
Jason Wu <jasonxw@uci.edu>
2021-04-22 19:03:54 -0700

    css, js fixes

--- WebContent/shopping-cart.js
+++ WebContent/shopping-cart.js
@@ -1,5 +1,23 @@
+let login_form = $("#login_form");
 
 function delete_row(e)
 {
     e.parentNode.parentNode.parentNode.parentNode.remove();
+}
+
+function handleLoginResult(resultDataString) {
+
+}
+
+function submitLoginForm(formSubmitEvent) {
+    console.log("submit login form");
+    formSubmitEvent.preventDefault();
+
+    $.ajax(
+        "api/login", {
+            method: "GET",
+            data: login_form.serialize(),
+            success: handleLoginResult
+        }
+    );
 }
