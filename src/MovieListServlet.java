707a68e2c0a7fbb11f76e7eb373a37f934ab7c03
Angelo Basa <ambasa@uci.edu>
2021-04-26 18:00:01 -0700

    bug fix with star ordering

--- src/MovieListServlet.java
+++ src/MovieListServlet.java
@@ -190,8 +190,8 @@ public class MovieListServlet extends HttpServlet {
             {
                 query = "SELECT movies.id, movies.title, movies.year, movies.director, r.rating, "
                         + " GROUP_CONCAT(DISTINCT g.name) as genre, "
-                        + " GROUP_CONCAT(DISTINCT ss.name ORDER BY (SELECT COUNT(*) FROM stars_in_movies z WHERE z.starId = s.id GROUP BY s.id ) DESC, s.name) as name,"
-                        + " GROUP_CONCAT(DISTINCT ss.id ORDER BY (SELECT COUNT(*) FROM stars_in_movies z WHERE z.starId = s.id GROUP BY s.id ) DESC, s.name) as nameId "
+                        + " GROUP_CONCAT(DISTINCT ss.name ORDER BY (SELECT COUNT(*) FROM stars_in_movies z WHERE z.starId = ss.id GROUP BY ss.id ) DESC, ss.name) as name,"
+                        + " GROUP_CONCAT(DISTINCT ss.id ORDER BY (SELECT COUNT(*) FROM stars_in_movies z WHERE z.starId = ss.id GROUP BY ss.id ) DESC, ss.name) as nameId "
                         + " FROM movies "
                         + " INNER JOIN(SELECT stars.id,stars.name, stars_in_movies.movieId FROM stars, stars_in_movies WHERE stars.id = stars_in_movies.starId and stars.name LIKE " +  "\"%" + n + "%\"" +  ") as s ON s.movieId = movies.id"
                         + " INNER JOIN(SELECT stars.id, stars.name, stars_in_movies.movieId FROM stars, stars_in_movies WHERE stars_in_movies.starId = stars.id ) as ss on ss.movieId = s.movieId"
