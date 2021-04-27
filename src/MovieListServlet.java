b06915381b8ff64d893584e328e84bd213978a66
Angelo Basa <ambasa@uci.edu>
2021-04-26 17:51:56 -0700

    Created a third query in movie list and ordered single star page by year

--- src/MovieListServlet.java
+++ src/MovieListServlet.java
@@ -166,8 +166,8 @@ public class MovieListServlet extends HttpServlet {
             // execute query
             String query = "";
 
-            //query for everything that isnt genre browsing
-            if(g == null) {
+            //query for everything that isnt genre browsing and name browsing
+            if(g == null && n == null ) {
                  query = "SELECT movies.id, movies.title, movies.year, movies.director, r.rating, GROUP_CONCAT(DISTINCT z.name) as genre, GROUP_CONCAT(DISTINCT s.name ORDER BY (SELECT COUNT(*) FROM stars_in_movies z WHERE z.starId = s.id GROUP BY s.id ) DESC, s.name) as name, GROUP_CONCAT(DISTINCT s.id ORDER BY (SELECT COUNT(*) FROM stars_in_movies z WHERE z.starId = s.id GROUP BY s.id ) DESC, s.name) as nameId"
                         + " FROM movies  INNER JOIN (SELECT ratings.movieId, ratings.rating FROM ratings) as r ON movies.id = r.movieId"
                         + " INNER JOIN( SELECT stars.id,stars.name, stars_in_movies.movieId FROM stars, stars_in_movies WHERE stars.id = stars_in_movies.starId) as s ON s.movieId = r.movieId"
@@ -177,14 +177,36 @@ public class MovieListServlet extends HttpServlet {
                 if (helper != "") {
                     query += " WHERE " + helper;
                 }
+                //if (helper2 != "") {
+               //     query += helper2;
+               // }
                 query += " GROUP BY movies.id ";
-                if (helper2 != "") {
-                    query += helper2;
-                }
                 query += " ORDER BY " + sortBy;
                 query += " LIMIT " + show + " OFFSET " + offsetAsString;
             }
 
+            //query for name browsing
+            else if(g== null && n!=null)
+            {
+                query = "SELECT movies.id, movies.title, movies.year, movies.director, r.rating, "
+                        + " GROUP_CONCAT(DISTINCT g.name) as genre, "
+                        + " GROUP_CONCAT(DISTINCT s.name ORDER BY (SELECT COUNT(*) FROM stars_in_movies z WHERE z.starId = s.id GROUP BY s.id ) DESC, s.name) as name,"
+                        + " GROUP_CONCAT(DISTINCT s.id ORDER BY (SELECT COUNT(*) FROM stars_in_movies z WHERE z.starId = s.id GROUP BY s.id ) DESC, s.name) as nameId "
+                        + " FROM movies "
+                        + " INNER JOIN(SELECT stars.id,stars.name, stars_in_movies.movieId FROM stars, stars_in_movies WHERE stars.id = stars_in_movies.starId and stars.name LIKE " +  "\"%" + n + "%\"" +  ") as s ON s.movieId = movies.id"
+                        + " INNER JOIN(SELECT stars.id, stars.name, stars_in_movies.movieId FROM stars, stars_in_movies WHERE stars_in_movies.starId = stars.id ) as ss on ss.movieId = s.movieId"
+                        + " INNER JOIN (SELECT ratings.movieId, ratings.rating FROM ratings) as r ON s.movieId = r.movieId  "
+                        + " INNER JOIN(SELECT genres.name,genres_in_movies.movieId FROM genres, genres_in_movies WHERE genres.id = genres_in_movies.genreId) as g on g.movieId = r.movieId ";
+                        if (helper != "") {
+                            query += " WHERE " + helper;
+                        }
+                        query += " GROUP BY movies.id ";
+                        query += " ORDER BY " + sortBy;
+                        query += " LIMIT " + show + " OFFSET " + offsetAsString;
+
+
+                System.out.println(query);
+            }
             //genre browsing query
             else{
 
