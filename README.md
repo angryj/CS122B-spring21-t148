8c095e6f26204ea927a135f0e599d978cb7dbd47
Jason Wu <jasonxw@uci.edu>
2021-05-06 14:20:11 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,34 @@
+### CS 122B Project 2
+Demo:https://www.youtube.com/watch?v=fGh6FQ5PXEQ
+
+### To run this:
+1. Clone the repo: https://github.com/UCI-Chenli-teaching/cs122b-spring21-team-148.git
+2. Run mvn package on the repo to create a .war file
+3. Populate the moviedb database using movie-data.sql and create_tables.sql and new-table.sql files (The steps are as follows)
+
+Create database moviedb
+
+use moviedb
+
+Source create_tables.sql
+
+Source movie-data.sql
+
+Source new-table.sql
+
+
+Go back to the repo directory and Use cp ./target/*.war /var/lib/tomcat9/webapps/ to push the .war file into tomcat
+
+Refresh tomcat on the web browser, and the deployed project should appear.
+
+### Substring Matching Design:
+For title, director, and star, we used the pattern % + search_term + % to get the mysql result we wanted.
+The logic for determining the correct query is located in the file MovieListServlet.java, lines 128-157
+
+### Contributions:
+
+Jason- Creating the Login page, and implementing the shopping cart 
+
+Angelo- Implementing the main page, extending Movie List page
+
+Both: Debugging, extending single movies and stars, general css tweaks
---
+++ WebContent/main.js
@@ -0,0 +1,102 @@
+let cart = $("#cart");
+
+/**
+ * Handle the data returned by IndexServlet
+ * @param resultDataString jsonObject, consists of session info
+ */
+function handleSessionData(resultDataString) {
+    let resultDataJson = JSON.parse(resultDataString);
+
+    console.log("handle session response");
+    console.log(resultDataJson);
+    console.log(resultDataJson["sessionID"]);
+
+    // show the session information
+    $("#sessionID").text("Session ID: " + resultDataJson["sessionID"]);
+    $("#lastAccessTime").text("Last access time: " + resultDataJson["lastAccessTime"]);
+
+    // show cart information
+    handleCartArray(resultDataJson["previousItems"]);
+}
+
+/**
+ * Handle the items in item list
+ * @param resultArray jsonObject, needs to be parsed to html
+ */
+function handleCartArray(resultArray) {
+    console.log(resultArray);
+    let item_list = $("#item_list");
+    // change it to html list
+    let res = "<ul>";
+    for (let i = 0; i < resultArray.length; i++) {
+        // each item will be in a bullet point
+        res += "<li>" + resultArray[i] + "</li>";
+    }
+    res += "</ul>";
+
+    // clear the old array and show the new array in the frontend
+    item_list.html("");
+    item_list.append(res);
+}
+
+/**
+ * Submit form content with POST method
+ * @param cartEvent
+ */
+function handleCartInfo(cartEvent) {
+    console.log("submit cart form");
+    /**
+     * When users click the submit button, the browser will not direct
+     * users to the url defined in HTML form. Instead, it will call this
+     * event handler when the event is triggered.
+     */
+    cartEvent.preventDefault();
+
+    $.ajax("api/index", {
+        method: "POST",
+        data: cart.serialize(),
+        success: resultDataString => {
+            let resultDataJson = JSON.parse(resultDataString);
+            //handleCartArray(resultDataJson["previousItems"]);
+        }
+    });
+    var x = document.getElementById("cart")
+    console.log(x.elements[0].value)
+    console.log(x.elements[1].value)
+    console.log(x.elements[2].value)
+    console.log(x.elements[3].value)
+
+    let parameters = [x.elements[0].value,x.elements[1].value,x.elements[2].value,x.elements[3].value]
+    console.log(parameters)
+
+    var p = ""
+    if(parameters[0] != "")
+    {
+        p += "Title="+parameters[0]+"&"
+    }
+    if(parameters[1] != "")
+    {
+        p += "Year="+parameters[1]+"&"
+    }
+    if(parameters[2] != "")
+    {
+        p += "Director="+parameters[2]+"&"
+    }
+    if(parameters[3] != "")
+    {
+        p += "Name="+parameters[3]+"&"
+    }
+
+    window.location.replace("movie-list.html?" + p)
+
+    // clear input form
+    cart[0].reset();
+}
+
+$.ajax("api/index", {
+    method: "GET",
+    success: handleSessionData
+});
+
+// Bind the submit action of the form to a event handler function
+cart.submit(handleCartInfo);
