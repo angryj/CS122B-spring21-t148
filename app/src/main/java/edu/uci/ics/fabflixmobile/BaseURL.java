aa0c35eefe512901c2b485a9f6296396dacf9546
Jason Wu <jasonxw@uci.edu>
2021-05-23 20:29:25 -0700

    final url for aws

--- app/src/main/java/edu/uci/ics/fabflixmobile/BaseURL.java
+++ app/src/main/java/edu/uci/ics/fabflixmobile/BaseURL.java
@@ -1,9 +1,9 @@
 package edu.uci.ics.fabflixmobile;
 
 public class BaseURL {
-    private final String host = "10.0.0.158";
+    private final String host = "18.225.32.88";
     private final String port = "8443";
-    private final String domain = "blah";
+    private final String domain = "Fabflix-148";
     //public final String baseURL = "https://" + host + ":" + port + "/" + domain;
     //modify the baseURL to the EC2 Instance when we submit, modify this file for your own convenience
     //until then
